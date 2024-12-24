02933707464fcebe94ddf56e792e82e837680e3b
oldme <tyyn1022@gmail.com>
2024-12-24 10:24:20 +0800

    words

--- app/word/internal/controller/words/words.go
+++ app/word/internal/controller/words/words.go
@@ -2,54 +2,46 @@
 package words
 
 import (
-    "context"
+	"context"
 
-    v1 "proxima/app/word/api/words/v1"
-    "proxima/app/word/internal/logic/words"
+	v1 "proxima/app/word/api/words/v1"
+	"proxima/app/word/internal/logic/words"
 
-    "github.com/gogf/gf/contrib/rpc/grpcx/v2"
-    "github.com/gogf/gf/v2/util/gconv"
+	"github.com/gogf/gf/contrib/rpc/grpcx/v2"
+	"github.com/gogf/gf/v2/util/gconv"
 )
 
-// Controller implements the gRPC server interface for word management
-// It acts as a bridge between the gRPC transport layer and business logic layer
 type Controller struct {
-    v1.UnimplementedWordsServer
-    words *words.Words // Business logic layer instance
+	v1.UnimplementedWordsServer
+	words *words.Words
 }
 
-// Register registers the words service with the gRPC server
-// This function is called during server initialization
 func Register(s *grpcx.GrpcServer) {
-    v1.RegisterWordsServer(s.Server, &Controller{
-        words: words.New(),
-    })
+	v1.RegisterWordsServer(s.Server, &Controller{
+		words: words.New(),
+	})
 }
 
-// Create handles word creation requests
-// It converts the gRPC request to business logic input and returns the new word's ID
 func (c *Controller) Create(ctx context.Context, req *v1.CreateReq) (res *v1.CreateRes, err error) {
-    id, err := c.words.Create(ctx, words.CreateInput{
-        Uid:        req.Uid,
-        Word:       req.Word,
-        Definition: req.Definition,
-    })
-    if err != nil {
-        return nil, err
-    }
-    return &v1.CreateRes{
-        Id: uint32(id),
-    }, nil
+	id, err := c.words.Create(ctx, words.CreateInput{
+		Uid:        req.Uid,
+		Word:       req.Word,
+		Definition: req.Definition,
+	})
+	if err != nil {
+		return nil, err
+	}
+	return &v1.CreateRes{
+		Id: uint32(id),
+	}, nil
 }
 
-// Get retrieves word information by ID
-// It returns the complete word entity including all its attributes
 func (c *Controller) Get(ctx context.Context, req *v1.GetReq) (res *v1.GetRes, err error) {
-    word, err := c.words.Get(ctx, req.Id)
-    if err != nil {
-        return nil, err
-    }
-    res = &v1.GetRes{}
-    err = gconv.Scan(word, &res.Words)
-    return
+	word, err := c.words.Get(ctx, req.Id)
+	if err != nil {
+		return nil, err
+	}
+	res = &v1.GetRes{}
+	err = gconv.Scan(word, &res.Words)
+	return
 }
--- app/word/internal/logic/words/words.go
+++ app/word/internal/logic/words/words.go
@@ -2,41 +2,34 @@
 package words
 
 import (
-    "context"
+	"context"
 
-    "proxima/app/word/internal/dao"
-    "proxima/app/word/internal/model/do"
-    "proxima/app/word/internal/model/entity"
+	"proxima/app/word/internal/dao"
+	"proxima/app/word/internal/model/do"
+	"proxima/app/word/internal/model/entity"
 )
 
-// Words implements the word management logic layer
 type Words struct{}
 
-// New creates and returns a new Words instance
 func New() *Words {
-    return &Words{}
+	return &Words{}
 }
 
-// CreateInput defines the required fields for creating a new word
 type CreateInput struct {
-    Uid        uint32 // User ID who created the word
-    Word       string // The word to be stored
-    Definition string // Definition or meaning of the word
+	Uid        uint32
+	Word       string
+	Definition string
 }
 
-// Create stores a new word with its associated information
-// Returns the new word's ID and any error encountered during creation
 func (*Words) Create(ctx context.Context, in CreateInput) (id int64, err error) {
-    return dao.Words.Ctx(ctx).Data(do.Words{
-        Uid:        in.Uid,
-        Word:       in.Word,
-        Definition: in.Definition,
-    }).InsertAndGetId()
+	return dao.Words.Ctx(ctx).Data(do.Words{
+		Uid:        in.Uid,
+		Word:       in.Word,
+		Definition: in.Definition,
+	}).InsertAndGetId()
 }
 
-// Get retrieves a word's complete information by its ID
-// Returns the word entity if found, or error if not found or on database errors
 func (*Words) Get(ctx context.Context, id uint32) (word *entity.Words, err error) {
-    err = dao.Words.Ctx(ctx).WherePri(id).Scan(&word)
-    return
+	err = dao.Words.Ctx(ctx).WherePri(id).Scan(&word)
+	return
 }
